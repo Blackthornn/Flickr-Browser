bb1c597ee493a4b944d344a374861c9f604957d4
The Blackthorn <emekaujah@hotmail.co.uk>
2019-05-21 06:44:46 +1000

    Initial commit

---
+++ app/src/main/java/com/theblackthorn/flickrbrowser/SearchActivity.kt
@@ -0,0 +1,25 @@
+package com.theblackthorn.flickrbrowser
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import kotlinx.android.synthetic.main.activity_main.*
+
+import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.activity_search.toolbar
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
